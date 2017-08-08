ba0faa358e1e6fac7c77438ae63e3eb5a6ef6887
Joe Keenan <joe@flyingdiver.com>
2017-08-08 18:17:14 -0400

    Adding Domia devices

--- RFPlayer.indigoPlugin/Contents/Server Plugin/plugin.py
+++ RFPlayer.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -244,7 +244,7 @@ class Plugin(indigo.PluginBase):
         retList =[]
         for address, data in sorted(self.knownDevices.iteritems()):
             if data['status'] == 'Available' and data['protocolMeaning'] == filter:
-                retList.append((address, data['description']))
+                retList.append((address, "%s: %s" % (address, data['description'])))
                
         retList.sort(key=lambda tup: tup[1])
         return retList
@@ -932,10 +932,73 @@ class Plugin(indigo.PluginBase):
 
     def domiaHandler(self, player, frameData):
 
-        devAddress = "DOMIA-" + frameData['infos']['id']
+        devAddress = "DOMIA-" + frameData['infos']['idMeaning']
 
         self.logger.debug(u"%s: Sensor frame received: %s" % (player.device.name, devAddress))
 
+        # make sure this device is in the list of known sensor devices
+        
+        if devAddress not in self.knownDevices:
+            self.logger.info("New Domia Device %s" % (devAddress))
+            self.knownDevices[devAddress] = { 
+                "status": "Available", 
+                "devices" : indigo.List(),
+                "protocol": frameData['header']['protocol'], 
+                "protocolMeaning": frameData['header']['protocolMeaning'], 
+                "infoType": frameData['header']['infoType'], 
+                "subType": frameData['infos']['subType'],
+                "description": frameData['infos']['subTypeMeaning'],
+            }
+            self.logger.debug(u"added new known device: %s = %s" % (devAddress, unicode(self.knownDevices[devAddress])))
+            
+        # Is this a configured device?
+        self.logger.threaddebug(u"%s: Update pending, checking knownDevices = %s" % (player.device.name, str(self.knownDevices[devAddress])))
+        
+        if not (self.knownDevices[devAddress]['status'] == 'Active'):             # not in use
+            self.logger.threaddebug(u"%s: Device %s not active, skipping update" % (player.device.name, devAddress))
+            return
+            
+        deviceList = self.knownDevices[devAddress]['devices']
+        for deviceId in deviceList:
+            if deviceId not in self.sensorDevices:
+                self.logger.error(u"Device configuration error - 'Active' device not in sensor list: %s" % (devAddress))
+                continue
+                
+            sensor = self.sensorDevices[deviceId]       
+            sensorState = frameData['infos']['qualifier']
+            self.logger.debug(u"%s: Updating sensor %s to %s" % (sensor.name, devAddress, sensorState))                        
+            sensor.updateStateOnServer('onOffState', bool(int(sensorState)))
+            sensor.updateStateOnServer('sensorValue', sensorState, uiValue=sensorState)
+
+    def configDomia(self, device):
+
+        configDone = device.pluginProps.get('configDone', False)
+        self.logger.debug(u" %s: configDomia, configDone = %s" % (device.name, str(configDone)))
+        
+        if configDone:
+            return
+
+        address = device.pluginProps['address']
+
+        self.logger.threaddebug(u"configDomia (1) for knownDevices[%s] = %s" % (address, str(self.knownDevices[address])))
+
+        self.knownDevices.setitem_in_item(address, 'status', "Active")
+        devices = self.knownDevices[address]['devices']
+        devices.append(device.id)
+        self.knownDevices.setitem_in_item(address, 'devices', devices)
+
+        self.logger.threaddebug(u"configDomia (2) for knownDevices[%s] = %s" % (address, str(self.knownDevices[address])))
+        
+        
+        device.name = address
+        device.replaceOnServer()
+
+        newProps = device.pluginProps
+        newProps["configDone"] = True
+        device.replacePluginPropsOnServer(newProps)
+
+        self.logger.info(u"Configured configDomia Sensor '%s' (%s) @ %s" % (device.name, device.id, address))
+       
     ########################################
 
     def owlHandler(self, player, frameData):
